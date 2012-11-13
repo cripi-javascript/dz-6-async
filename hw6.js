51e9c51e92756d5182704222290cfd76d64ac85c
Ruslan Talipov <newroosh90@gmail.com>
2012-11-13 12:47:54 +0600

    заготовка

---
+++ hw6.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+var data = {
+    name: "pewpew",
+    start: new Date(),
+    end: new Date()
+};
+
+var collection = [data];
+
+var json = JSON.stringify(collection);
+
+function getEvent(){};
+
+function postEvent(){};

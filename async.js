7d336b054243c58c8acbd805a851bafbf456da1f
Natalya Bronina <dimitrievna@gmail.com>
2012-11-24 19:06:36 +0600

    проверка чтения из файла

---
+++ async.js
@@ -0,0 +1,13 @@
+﻿var xhr = new XMLHttpRequest();
+// Подготавливаем запрос
+xhr.open('GET', 'test.json', true);
+// Подписываемся на событие "изменение статуса"
+xhr.addEventListener('readystatechange', function () {
+// Когда ответ пришел
+if (xhr.readyState === 4) {
+// Печатаем тело ответа
+console.log(xhr.responseText);
+}
+}, false);
+// Отправляем запрос
+xhr.send();
